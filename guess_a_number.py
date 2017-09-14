97e4ddb7a10102ebb5e2fe16c5c7b874b6d32c04
Heinle <jheinl3766@greenville.k12.sc.us>
2017-09-14 09:26:02 -0400

    Made tries

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,21 +4,30 @@ import random
 
 low = 1
 high = 100
+tries = 5
 
 rand = random.randrange(low, high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
 
-while guess != rand:
+while guess != rand and tries > 0:
     guess = input("Take a guess: ")
     guess = int(guess)
     
     if guess < rand:
         print("You guessed too low.")
+        tries -= 1
+        print("You have " + str(tries) + " tries left.")
     elif guess > rand:
         print("You guessed too high.")
+        tries -= 1
+        print("You have " + str(tries) + " tries left.")
     else:
-        print("You got it!")
+        print("You got it fam!")
 
-print("Game over")
+
+if guess == rand:
+    print("Good Job Fam")
+else:
+    print("Ur Bad Kid")
