11aab01b10ca0fae4ad6e376ae878979b157ba3d
Heinle <jheinl3766@greenville.k12.sc.us>
2017-09-18 09:16:59 -0400

    made is retry

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,13 +4,7 @@ import random
 
 low = 1
 high = 100
-tries = 5
-
-# start game
-rand = random.randrange(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
-
-guess = -1
+limit = 8
 
 # helper functions
 def get_guess():
@@ -23,23 +17,52 @@ def get_guess():
         else:
             print("I don't understand medicine.")
 
-# play game
-while guess != rand and tries > 0:
-    guess = get_guess()
-    
-    if guess < rand:
-        print("You guessed too low.")
-        tries -= 1
-        print("You have " + str(tries) + " tries left.")
-    elif guess > rand:
-        print("You guessed too high.")
-        tries -= 1
-        print("You have " + str(tries) + " tries left.")
+def play_again():
+    while True:
+        decision = input("You wanna play again? (y/n)")
+
+        if decision == "y" or decision == "yes":
+            return True
+        elif decision == "n" or decision == "no":
+            return False
+
+        print("Type y or n dummy.")
+
+again = True
+
+while again:
+    # start game
+    tries = limit
+    rand = random.randrange(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+
+    guess = -1
+
+    # play game
+    while guess != rand and tries > 0:
+        guess = get_guess()
+        
+        if guess < rand:
+            print("You guessed too low.")
+            tries -= 1
+            print("You have " + str(tries) + " tries left.")
+        elif guess > rand:
+            print("You guessed too high.")
+            tries -= 1
+            print("You have " + str(tries) + " tries left.")
+        else:
+            print("You got it fam!")
+
+
+    if guess == rand:
+        print("Good Job Fam")
     else:
-        print("You got it fam!")
+        print("Ur Bad Kid. The number was " + str(rand) + ".")
+
+    again = play_again()
+
+print("Bye bye my dude.")
+
+    
 
 
-if guess == rand:
-    print("Good Job Fam")
-else:
-    print("Ur Bad Kid")
