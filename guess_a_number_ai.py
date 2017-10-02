ea8d62ebea372a8d8de815e3542998421da1d209
Heinle <jheinl3766@greenville.k12.sc.us>
2017-10-02 09:27:04 -0400

    added guess limit

--- guess_a_number_ai.py
+++ guess_a_number_ai.py
@@ -48,9 +48,8 @@ def get_guess(current_low, current_high):
     return guess
 
 def pick_number():
-    print("Ey fam, Pick a number between " + str(low) + " and " + str(high) + " and i'm gunna guess it.")
+    guess = input("Ey fam, Pick a number between " + str(low) + " and " + str(high) + " and i'm gunna guess it.")
     print()
-    guess = input("Press Enter to continue...")
     if int(guess) >= high + 1:
         print("Between " + str(low) + " and " + str(high) + " you dum dum")
         pick_number()
@@ -68,19 +67,23 @@ def check_guess(guess, aiguess, limit):
     print()
     if playinpu == "l" or playinpu == "low":
         check =  -1
-        return check, aiguess
+        return check
     elif playinpu == "h" or playinpu == "high":
         check = 1
-        return check, aiguess
-    elif playinpu == "correct" or playinpu == "right" or playinpu == "yes":
+        return check
+    elif playinpu == "correct" or playinpu == "right" or playinpu == "yes" or playinpu == "c":
         check = 0
-        return check, aiguess
+        return check
     else:
         print("Type High Or Low")
         
 
-def show_result(guess, aiguess):
-    print("I know good and well your number was " + str(guess) + ".")
+def show_result(guess, aiguess, limit):
+    if aiguess != limit:
+        print("I know good and well your number was " + str(guess) + ".")
+        print("I got it in " + aiguess + " guesses.")
+    else:
+        print("I suck and should be terminated")
     print()
     print()
 
@@ -96,25 +99,31 @@ def play_again():
             
 
 def play():
-    limit = math.ceil(math.log(high - low + 1 , 2))
-    aiguess = 1
     current_low = low
     current_high = high
     check = -1
+    aiguess = 1
+    limit = math.ceil(math.log(high - low + 1 , 2)) + 1
+
+    input("Press Enter to continue...")
 
     pick_number()
     
     while check != 0:
         guess = get_guess(current_low, current_high)
-        check, ainumber = check_guess(guess, aiguess , limit)
-        ainumber = ainumber + 1
-        
-        if check == -1:
-            current_low = guess
-        elif check == 1:
-            current_high = guess
-        elif check == 0:
-            pass
+        check = check_guess(guess, aiguess , limit)
+        aiguess = aiguess + 1
+
+        if aiguess != limit:
+            if check == -1:
+                current_low = guess
+            elif check == 1:
+                current_high = guess
+            elif check == 0:
+                pass
+        else:
+            print("I ran out of guesses...rip")
+            check = 0
 
     show_result(guess, aiguess, limit)
 
